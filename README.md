220fa019bd9c2054248e9f2a9a723fd8b1e1cd38
Phillip Cloud <cpcloud@gmail.com>
2017-07-25 22:36:31 -0400

    ENH: Rewrite

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Build arbitrary conda dependency graphs from source
 Here's an example of how to use `scourge`.
 
 
-### Initialization: `scourge init`
+### 1. Initialization: `scourge init`
 
 Right now all this does is pull down the `condaforge/linux-anvil` docker image.
 
@@ -20,28 +20,43 @@ Digest: sha256:dd53ef1792f777fa8b2abf30221bf9beb6cb6a81d158d498f0219c4d475696cd
 Status: Image is up to date for condaforge/linux-anvil:latest
 ```
 
-### `Makefile` generation: `scourge gen <packages>`
+### 2. Feedstock clone: `scourge clone <packages>`
 
-This writes to standard out a `Makefile` which will build your packages in
-topological order. `scourge` just generates the package name and its dependency
-list and `make` does the sorting.
+This step clones each package's feedstock from the `conda-forge` GitHub
+repository into the current working directory.
 
 ```sh
-$ scourge gen parquet-cpp arrow-cpp pyarrow
-all: parquet-cpp arrow-cpp pyarrow
+$ scourge clone parquet-cpp arrow-cpp pyarrow
+```
+
+There's no output if cloning is successful.
+
+### 3. `Makefile` generation: `scourge gen`
+
+This writes to standard out (or a file name) a `Makefile` which will
+build your packages in topological order.
+
+`scourge` generates the package name and its dependency list and `make` does
+the sorting.
+
+```sh
+$ scourge gen
+all: ./artifacts/pyarrow ./artifacts/parquet-cpp ./artifacts/arrow-cpp
+
+clean:
+        rm -rf ./artifacts
+
+./artifacts/pyarrow: ./artifacts/arrow-cpp ./artifacts/parquet-cpp
+        pyarrow-feedstock/ci_support/run_docker_build.sh
+        cp $(wildcard pyarrow-feedstock/build_artefacts/pyarrow*.tar.bz2) $@
 
 
-parquet-cpp: arrow-cpp
-        [ ! -d "$@-feedstock" ] && git clone git://github.com/conda-forge/$@-feedstock
-        $@-feedstock/ci_support/run_docker_build.sh
+./artifacts/parquet-cpp: ./artifacts/arrow-cpp
+        parquet-cpp-feedstock/ci_support/run_docker_build.sh
+        cp $(wildcard parquet-cpp-feedstock/build_artefacts/parquet-cpp*.tar.bz2) $@
 
 
-arrow-cpp: parquet-cpp
-        [ ! -d "$@-feedstock" ] && git clone git://github.com/conda-forge/$@-feedstock
-        $@-feedstock/ci_support/run_docker_build.sh
-
-
-pyarrow: arrow-cpp parquet-cpp
-        [ ! -d "$@-feedstock" ] && git clone git://github.com/conda-forge/$@-feedstock
-        $@-feedstock/ci_support/run_docker_build.sh
+./artifacts/arrow-cpp: 
+        arrow-cpp-feedstock/ci_support/run_docker_build.sh
+        cp $(wildcard arrow-cpp-feedstock/build_artefacts/arrow-cpp*.tar.bz2) $@
 ```
--- scourge/scourge.py
+++ scourge/scourge.py
@@ -1,13 +1,19 @@
-import uuid
-import json
+import os
+import pathlib
+import io
 
-from functools import partial
 from multiprocessing import cpu_count
 from concurrent import futures
 
+import yaml
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
 import click
 import sh
-import toolz
 
 
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
@@ -24,44 +30,87 @@ def init(docker_image):
     click.echo(sh.docker.pull(docker_image, _fg=True))
 
 
-@cli.command(help='Generate a Makefile for building packages')
+@cli.command(help='Clone package feedstocks to build')
 @click.argument('packages', nargs=-1)
-def gen(packages):
-    command = sh.conda.create.bake(
-        name=uuid.uuid4().hex,
-        dry_run=True,
-        json=True,
-        channel='conda-forge'
-    )
-
-    get_package_data = toolz.compose(json.loads, str, command)
-
-    graph = {package: set() for package in packages}
-
+def clone(packages):
     with futures.ThreadPoolExecutor(
         max_workers=min(cpu_count(), len(packages))
-    ) as e:
-        results = futures.as_completed(
-            map(partial(e.submit, get_package_data), packages),
-        )
-        for package, package_data in zip(
-            packages,
-            (future.result() for future in results)
+    ) as executor:
+        for future in futures.as_completed(
+            executor.submit(
+                sh.git.clone,
+                'git://github.com/conda-forge/{}'.format(feedstock),
+            ) for feedstock in map('{}-feedstock'.format, packages)
+            if not os.path.exists(feedstock)
         ):
-            action, *_ = package_data['actions']
-            link = action['LINK']
-            for dep in link:
-                dep_name = dep['name']
-                if dep_name in packages and dep_name != package:
-                    graph[package].add(dep_name)
+            try:
+                future.result()
+            except Exception as e:
+                raise click.ClickException(str(e))
+
+
+@cli.command(help='Generate a Makefile for building packages')
+@click.argument(
+    'output',
+    required=False,
+    default=click.get_text_stream('stdout'),
+    type=click.File('wt'),
+)
+@click.option(
+    '--package-dir',
+    default='.',
+    type=click.Path(exists=True),
+    help='Directory of feedstocks',
+)
+@click.option(
+    '--artifact-dir',
+    default=os.path.join('.', 'artifacts'),
+    type=click.Path(),
+    help='Where to place tarballs upon building successfully',
+)
+def gen(output, package_dir, artifact_dir):
+    packages = {
+        p.stem[:-len('-feedstock')]: p
+        for p in pathlib.Path(package_dir).glob('*-feedstock') if p.is_dir()
+    }
+
+    graph = {package_name: set() for package_name in packages.keys()}
+
+    for package_name, package in packages.items():
+        result = io.StringIO(str(
+            sh.conda.render(
+                str(package / 'recipe' / 'meta.yaml'),
+            )
+        ))
+        meta = yaml.load(result, Loader=Loader)
+        requirements = meta['requirements']
+
+        for dep in frozenset(requirements['build'] + requirements['run']):
+            raw_dep, *_ = dep.split(' ', 1)
+            if raw_dep in packages:
+                graph.setdefault(package_name, set()).add(raw_dep)
+
+        os.makedirs(os.path.join(artifact_dir, package_name), exist_ok=True)
+
+    target_template = '{}/{{}}'.format(artifact_dir)
+
     template = """
-{}: {}
-\t[ ! -d "$@-feedstock" ] && git clone git://github.com/conda-forge/$@-feedstock
-\t$@-feedstock/ci_support/run_docker_build.sh
-"""
-    lines = ['all: {}\n'.format(' '.join(graph.keys()))]
+{}/{{package_name}}: {{deps}}
+\t{{stem}}/ci_support/run_docker_build.sh
+\tcp $(wildcard {{stem}}/build_artefacts/{{package_name}}*.tar.bz2) $@
+""".format(artifact_dir)
+    lines = [
+        'all: {}\n'.format(
+            ' '.join(map(target_template.format, graph.keys()))
+        ),
+        'clean:\n\trm -rf {}'.format(artifact_dir)
+    ]
     lines += [
-        template.format(node, ' '.join(edges))
+        template.format(
+            package_name=node,
+            deps=' '.join(map(target_template.format, edges)),
+            stem='{}-feedstock'.format(node),
+        )
         for node, edges in graph.items()
     ]
-    click.echo('\n'.join(lines))
+    output.write('\n'.join(lines))
