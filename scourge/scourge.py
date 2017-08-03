30b44a16e49fd035df2f18b8b9e846be51b91f9b
Phillip Cloud <cpcloud@gmail.com>
2017-08-03 13:43:38 -0400

    keys

--- scourge/scourge.py
+++ scourge/scourge.py
@@ -480,7 +480,7 @@ def build(constraints, jobs, environment):
         '-e', 'HOST_USER_ID={:d}'.format(os.getuid()),
     )
 
-    tasks = {package: [] for (package, _, _), _ in scripts}
+    tasks = {package: [] for package, _, _ in scripts.keys()}
 
     for (package, python, numpy), script in scripts.items():
         task_args = functools.reduce(
